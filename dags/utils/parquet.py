b9076f646eea12b495bd4867265b83ddbec7984e
Leandro Tiburske <tiburskeleandro@gmail.com>
2026-06-05 12:37:53 +0200

    Document utils functions

--- dags/utils/parquet.py
+++ dags/utils/parquet.py
@@ -3,6 +3,14 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 
 def records_to_parquet_bytes(records: list[dict]) -> bytes:
+    """
+    Convert a list of dictionaries to a parquet file in bytes.
+    
+    :param records: A list of dictionaries to be converted to parquet format.
+    :type records: list[dict]
+    :return: The parquet file as bytes.
+    :rtype: bytes
+    """
     table = pa.Table.from_pylist(records)
     buffer = io.BytesIO()
     pq.write_table(table, buffer, compression="snappy")
--- dags/utils/s3.py
+++ dags/utils/s3.py
@@ -3,11 +3,38 @@ import boto3
 import io
 import json
 
-def generate_s3_key(prefix: str, filename: str) -> str:
+def generate_s3_key(
+        prefix: str,
+        filename: str
+    ) -> str:
+    """
+    Generate an S3 key with a timestamp.
+    
+    :param prefix: Prefix for the S3 key (e.g., "raw/models")
+    :type prefix: str
+    :param filename: Filename for the S3 key
+    :type filename: str
+    :return: The generated S3 key
+    :rtype: str
+    """
     date_str = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     return f"{prefix}/{date_str}/{filename}"
 
-def upload_to_s3(bucket_name: str, key: str, data: dict):
+def upload_to_s3(
+        bucket_name: str,
+        key: str,
+        data: dict
+    ) -> None:
+    """
+    Upload data to S3.
+    
+    :param bucket_name: Name of the S3 bucket
+    :type bucket_name: str
+    :param key: S3 key for the object
+    :type key: str
+    :param data: Data to upload
+    :type data: dict
+    """
     s3 = boto3.client("s3")
     s3.put_object(
         Bucket=bucket_name,
@@ -19,7 +46,19 @@ def upload_parquet_to_s3(
     key: str,
     data: bytes,
     content_type: str = "application/vnd.apache.parquet",
-):
+    ) -> None:
+    """
+    Upload Parquet data to S3.
+    
+    :param bucket_name: Name of the S3 bucket
+    :type bucket_name: str
+    :param key: S3 key for the object
+    :type key: str
+    :param data: Parquet data to upload
+    :type data: bytes
+    :param content_type: Content type for the S3 object
+    :type content_type: str
+    """
     s3 = boto3.client("s3")
     s3.put_object(
         Bucket=bucket_name,
@@ -27,7 +66,20 @@ def upload_parquet_to_s3(
         Body=data,
         ContentType=content_type)
 
-def retrieve_from_s3(bucket_name, s3_key):
+def retrieve_from_s3(
+        bucket_name: str,
+        s3_key: str
+    ) -> dict:
+    """
+    Retrieve data from S3.
+
+    :param bucket_name: Name of the S3 bucket
+    :type bucket_name: str
+    :param s3_key: S3 key for the object
+    :type s3_key: str
+    :return: The retrieved data
+    :rtype: dict
+    """
     s3 = boto3.client("s3")
     response = s3.get_object(Bucket=bucket_name, Key=s3_key)
     return json.loads(response['Body'].read().decode('utf-8'))
